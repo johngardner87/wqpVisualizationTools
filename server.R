3d92ad0d056ad8ce028f78889cdac5a6bd45ddc9
Yoav Kargon <yoav.kargon@gmail.com>
2019-06-12 15:52:51 -0400

    Worked on selection process for HUC boundaries, updated UI

--- server.R
+++ server.R
@@ -12,26 +12,22 @@ library(tmap)
 # Server ------------------------------------------------------------------
 
 setwd("~/Documents/School/Duke/Summer 2019/Data+/")
-# measByLocGeoms <- st_read("Datasets/wqp_chlorophyll.gpkg")
-
 function(input, output, session) {
-  
-  wbd_path <- "Datasets/WBD_National_GDB/WBD_National_GDB.gdb/"
   
   # tmap_mode("view")
   
   output$wqpMap <- renderLeaflet({
     
     # Bounds fit continental US
-    leaflet() %>% addTiles() %>% fitBounds(-125, 25, -70, 50)
+    leaflet() %>% addProviderTiles(providers$Esri.WorldGrayCanvas) %>% fitBounds(-100, 25, -75, 55)
     
     # map <- tm_shape(bounds) + tm_polygons("MAP_COLORS")
     # tmap_leaflet(map)
-      
   })
   
   observe({
     hucLevel <- input$hucInput
+    hucColumn <- paste("HUC", hucLevel, sep="")
     constituents <- input$constInput
     
     boundaries <- paste("WBDHU", hucLevel, "Counts", ".gpkg", sep = "") %>% 
@@ -39,23 +35,54 @@ function(input, output, session) {
       st_read()
     
     bins <- c(0, 1000, 3000, 6000, 20000, 100000, Inf)
-    # pal <- colorBin("YlOrRd", domain=boundaries$chlorophyllMeasCount, bins = bins)
-    pal <- colorBin("PuBu", domain=boundaries$chlorophyllMeasCount, bins = bins)
+    pal <- colorBin("YlOrRd", domain=boundaries$chlorophyllMeasCount, bins = bins)
+    # pal <- colorBin("PuBu", domain=boundaries$chlorophyllMeasCount, bins = bins)
     
     leafletProxy("wqpMap", data = boundaries) %>%
       clearShapes() %>%
       addPolygons(fillColor = ~pal(chlorophyllMeasCount), #topo.colors(10)
-                  stroke = FALSE,
-                  fillOpacity = 0.6,
+                  color = "black",
+                  weight = 1,
+                  opacity = 1,
+                  fillOpacity = 0.7,
                   highlight = highlightOptions(
-                    weight=2,
-                    color = "#666",
+                    weight = 5,
+                    color = "black",
                     fillOpacity = 1,
-                    bringToFront = TRUE),
+                    bringToFront = TRUE,
+                    sendToBack = TRUE),
                   label = paste(boundaries$NAME, ": ", boundaries$chlorophyllMeasCount, " measurements", sep=""),
+                  
+                  layerId = boundaries[[hucColumn]],
+                  
                   labelOptions = labelOptions(
                     textsize = "12px"
                   )
       )
+    
+    hucSelected <- -1
+    print("reset")
+    observeEvent(input$wqpMap_shape_click, {
+      
+      event <- input$wqpMap_shape_click
+      if (event$id != hucSelected) {
+        oldHuc <- hucSelected
+        hucSelected <<- event$id
+        selectedHucBound <- boundaries %>% 
+          filter(.data[[hucColumn]] == hucSelected)
+        leafletProxy("wqpMap", data=selectedHucBound) %>% 
+          # {if (hucSelected != -1) {removeShape(as.character(selectedHucBound[[hucColumn]]))}} %>%
+          removeShape(paste(oldHuc, "Selected", sep="")) %>% 
+          addPolygons(layerId = paste(selectedHucBound[[hucColumn]], "Selected", sep=""),
+                      fillColor = ~pal(chlorophyllMeasCount),
+                      color = "red",
+                      weight = 3,
+                      opacity = 1
+          )
+        print(class(event$id))
+        print(paste("you've selected: ", event$id, sep=""))
+      }
+    })
   })
+  
 }
--- ui.R
+++ ui.R
@@ -12,28 +12,24 @@ library(tmap)
 
 fluidPage(
   
-  titlePanel("Water Quality Portal"),
+  # titlePanel("Water Quality Portal"),
   
   # Map Output
-  leafletOutput(outputId = "wqpMap"),
+  leafletOutput(outputId = "wqpMap", height = 800),
   
-  selectInput(inputId = "hucInput",
-              label = "HUC Level",
-              choices = c(2, 4, 6, 8, 10, 12)),
-  
-  selectInput(inputId = "constInput",
-              label = "Constituent",
-              choices = c("Chlorophyll" = "chlorophyll", "Dissolved Oxygen Concentration (DOC)" = "doc", "Turbidity (secchi)" = "secchi", "Total Suspended Solids (tss)" = "tss"),
-              multiple = T,
-              selected = "chlorophyll")
-  # fluidRow(
-  #   
-  #   column(4,
-  #     
-  #     
-  #     
-  #   ),
-  #   
-  # )
-  
+  absolutePanel(id = "Inputs", fixed = TRUE, draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                width = 200, height = "auto", class = "pan",
+                
+                h2("Water Quality Portal"),
+                
+                selectInput(inputId = "hucInput",
+                            label = "HUC Level",
+                            choices = c(2, 4, 6, 8, 10, 12)),
+                
+                selectInput(inputId = "constInput",
+                            label = "Constituent",
+                            choices = c("All", "Chlorophyll" = "chlorophyll", "Dissolved Oxygen Concentration (DOC)" = "doc", "Turbidity (secchi)" = "secchi", "Total Suspended Solids (tss)" = "tss"),
+                            multiple = F,
+                            selected = "chlorophyll")
+  )
 )
